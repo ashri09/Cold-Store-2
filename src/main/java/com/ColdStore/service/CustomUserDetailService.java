51e4dabf5d9afa3d4c825f436788dd596dd16669
ashri09 <ashrimandu@gmail.com>
2023-08-10 21:25:10 +0545

    taja taja

---
+++ src/main/java/com/ColdStore/service/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package com.ColdStore.service;
+
+import com.ColdStore.repo.UserRepository;
+import com.ColdStore.entity.CustomUserDetail;
+import com.ColdStore.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(email);
+        user.orElseThrow(() -> new UsernameNotFoundException("User Vetena"));
+        return user.map(CustomUserDetail::new).get();
+
+    }
+}
---
+++ src/test/java/com/application/Coldstore/ColdStoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.application.Coldstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColdStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
